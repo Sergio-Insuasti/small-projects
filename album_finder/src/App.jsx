ba8a975d0fcc9ed662ddc8a5d7ebf65408bd8a12
WallOfNoise <z5338374@ad.unsw.edu.au>
2025-09-16 00:59:08 +1000

    Completed album_finder project

---
+++ album_finder/src/App.jsx
@@ -0,0 +1,168 @@
+import "./App.css";
+import {
+  FormControl,
+  InputGroup,
+  Container,
+  Button,
+  Card,
+  Row,
+} from "react-bootstrap";
+import { useState, useEffect } from "react";
+
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+
+function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    let authParams = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        clientId +
+        "&client_secret=" +
+        clientSecret,
+    };
+
+    fetch("https://accounts.spotify.com/api/token", authParams)
+      .then((result) => result.json())
+      .then((data) => {
+        setAccessToken(data.access_token);
+      });
+  }, []);
+
+  async function search() {
+    let artistParams = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    };
+
+    // Get Artist
+    const artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+      artistParams
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        return data.artists.items[0].id;
+      });
+
+    // Get Artist Albums
+    await fetch(
+      "https://api.spotify.com/v1/artists/" +
+        artistID +
+        "/albums?include_groups=album&market=US&limit=50",
+      artistParams
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        setAlbums(data.items);
+      });
+  }
+
+  return (
+    <>
+      <Container>
+        <InputGroup>
+          <FormControl
+            placeholder="Search For Artist"
+            type="input"
+            aria-label="Search for an Artist"
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                search();
+              }
+            }}
+            onChange={(event) => setSearchInput(event.target.value)}
+            style={{
+              width: "300px",
+              height: "35px",
+              borderWidth: "0px",
+              borderStyle: "solid",
+              borderRadius: "5px",
+              marginRight: "10px",
+              paddingLeft: "10px",
+            }}
+          />
+          <Button onClick={search}>Search</Button>
+        </InputGroup>
+      </Container>
+
+      <Container>
+        <Row
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+            alignContent: "center",
+          }}
+        >
+          {albums.map((album) => {
+  const imgUrl = album.images?.[0]?.url;
+  const albumUrl = album.external_urls?.spotify;
+
+  return (
+    <Card
+      key={album.id}
+      style={{
+        backgroundColor: "#101826",
+        margin: "10px",
+        borderRadius: "5px",
+        marginBottom: "30px",
+      }}
+    >
+      {/* Make artwork the link */}
+      <a
+        href={albumUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ display: "block" }}
+        aria-label={`Open ${album.name} on Spotify`}
+      >
+        <Card.Img
+          width={200}
+          src={imgUrl}
+          alt={album.name}
+          style={{ borderRadius: "4%", cursor: "pointer" }}
+        />
+      </a>
+
+      <Card.Body>
+        <Card.Title
+          style={{
+            whiteSpace: "wrap",
+            fontWeight: "bold",
+            maxWidth: "200px",
+            fontSize: "18px",
+            marginTop: "10px",
+            color: "white",
+          }}
+        >
+          {album.name}
+        </Card.Title>
+
+        <Card.Text style={{ color: "white" }}>
+          Release Date: <br /> {album.release_date}
+        </Card.Text>
+        {/* Removed the Button since artwork is the link now */}
+      </Card.Body>
+    </Card>
+  );
+})}
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default App;
